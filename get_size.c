d681fd680574658d880ede092dd9a0995c89ea49
YangaRubushe <zilithemba@10>
2023-08-18 17:46:44 -0700

    Get Size

--- get_size.c
+++ get_size.c
@@ -13,18 +13,18 @@ int get_size(const char *format, int *i)
 	int curr_i = *i + 1;
 	int size = 0;
 
-	// Check for 'l' modifier (long)
+	/* Check for 'l' modifier (long) **/
 	if (format[curr_i] == 'l')
 		size = S_LONG;
-	// Check for 'h' modifier (short)
+	/* Check for 'h' modifier (short) **/
 	else if (format[curr_i] == 'h')
 		size = S_SHORT;
 
-	//If no size modifier found, reset the index to the original position
+	/** If no size modifier found, reset the index to the original position */
 	if (size == 0)
 		*i = curr_i - 1;
 	else
 		*i = curr_i;
 
-	return size;
+	return (size);
 }
