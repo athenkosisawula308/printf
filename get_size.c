94162633d4b7a5e104cb8b8d6454eb0ad378c10a
YangaRubushe <zilithemba@10>
2023-08-18 17:32:19 -0700

    get_size function for handling size modifiers in format string

---
+++ get_size.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * get_size - Determines the necessary casting size for the argument.
+ * @format: Formated string used for printing arguments.
+ * @i: List of arguments to be printed.
+ *
+ * Return: The precision value.
+ */
+
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	// Check for 'l' modifier (long)
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	// Check for 'h' modifier (short)
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	//If no size modifier found, reset the index to the original position
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return size;
+}
